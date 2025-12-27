eb481db5fb2d50aae6f3109cd4d39d161b19ae3b
ssimran4672-collab <ssimran4672@gmail.com>
2025-12-27 05:26:14 +0000

    container confige done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "abc@abc.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2
+ console.log(accountId);
+ accountEmail = "xyz@xyz.com"
+ accountPassword = "121212"
+
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
