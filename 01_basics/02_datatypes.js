7cf514154103a88d9c57e0bec5caed7ac14d05b4
ssimran4672-collab <ssimran4672@gmail.com>
2026-01-02 05:50:21 +0000

    conersion

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,3 +4,26 @@
 console.log(3 + 3)
 
 console.log("hitesh")
+
+let name = "Simran"        // string datatype
+let age = 18              // number datatype
+let isLoggedIs = false    // boolean
+
+
+//     Primitive 
+// number => 2^53
+// bigInt => for large number
+// string => ""
+// boolean => true/false
+// null => standlone valuse 
+// undefined =>  abhi valuse assigned nhi huui hai
+// symbol => unique
+
+//object 
+// null ek object hai
+
+console.log(typeof"Simran")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof age )
+
