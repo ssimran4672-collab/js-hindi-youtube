e098b3cc866c592e76d2bebc1f64814ce7822d88
ssimran4672-collab <ssimran4672@gmail.com>
2026-01-03 06:35:45 +0000

    conversion

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -18,8 +18,25 @@ console.log(booleanIsLoggedIn);
 //"" => false
 // "Simran"=> true
 
-let someNumber = 33
+letsomeNumber = "33"
  
 let stringNumber =String(someNumber)
 console.log(stringNumber)
 console.log( typeof stringNumber)
+
+
+//************operation */
+
+
+let value = "3"
+let negValue = -value
+console.log(negValue);
+
+console.log([2+2],[2-2],[2*2],[2**3])
+
+let str1 = "hello"
+let str2 = "Simran"
+
+let str3 = str1 + str2
+
+console.log(str3)
---
+++ 01_basics/03_dataconversionOperation.js
@@ -0,0 +1,41 @@
+
+let value = "3"
+let negValue = -value
+console.log(negValue);
+
+console.log([2+2],[2-2],[2*2],[2**3])
+
+let str1 = "hello"
+let str2 = " Simran"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log( "1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true);
+
+// true=> true
+//+true => 1
+//+"" => 0
+
+let gameCounter = 100
+gameCounter++
+console.log(gamecounter);
+
+
+let a = 3
+a++
+console.log(a++);
+
+let b = 3
+++b 
+console.log(++b);
+
+
+// prefix => 
+// postfix =>
